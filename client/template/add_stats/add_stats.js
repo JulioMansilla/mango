eb3cfa51a44d324ba731b9d530aace39595d917c
Ronnie Simonelli <ronnie.simonelli@gmail.com>
2015-10-25 01:33:54 -0400

    Add helper function for finding form data

--- client/template/add_stats/add_stats.js
+++ client/template/add_stats/add_stats.js
@@ -12,48 +12,22 @@ Template.addStats.helpers({
 Template.addStats.events({
   "click #save-stats": function(event, template){
     event.preventDefault();
-    
-    userStats = getAttributesFromForm();
 
-    UserStats.update(Session.get("myStats")._id, userStats, { upsert: true} );
+    userStats = getAttributesFromForm(template, "stats");
+    Meteor.users.update({ _id: Meteor.userId() }, {$set: {profile: {userStats: userStats}}})
 
     $('#addStats').modal('hide');
-
-    function getAttributesFromForm(){
-      userStats = {
-        "userId": Meteor.userId(),
-        "bab":   template.find("#bab").value,
-        "str":   template.find("#str").value,
-        "dex":   template.find("#dex").value,
-        "con":   template.find("#con").value,
-        "lvl":   template.find("#lvl").value,
-        "intl":  template.find("#int").value,
-        "wis":   template.find("#wis").value,
-        "cha":   template.find("#cha").value
-      }
-      return userStats;
-    }
   },
+
   "click #save-attacks": function(event, template){
     event.preventDefault();
 
-    userAttacks = getAttacksFromForm();
-    UserAttacks.update(Session.get("myAttacks")._id, userAttacks, {upsert: true} );
+    userAttacks = getAttributesFromForm(template, "attack");
+    userAttacks["userId"] = Meteor.userId();
+
+    UserAttacks.insert(userAttacks);
+
     $('#addAttacks').modal('hide');
-    function getAttacksFromForm(){
-      userAttacks = {
-        "userId":         Meteor.userId(),
-        "attackName":     template.find("#attackName").value,
-        "damage":         template.find("#damage").value, 
-        "critRange":      template.find("#critRange").value,
-        "critMultiplier": template.find("#critMultiplier").value,
-        "wepType":        template.find("#wepType").value,
-        "dmgMultiplier":  template.find("#dmgMultiplier").value,
-        "range":        template.find("#range").value,
-        "ammo":        template.find("#ammo").value
-      }
-      return userAttacks;
-    }
   }
 });
 
